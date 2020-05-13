5eff8b79261625daa065ac5f95e2ce2c964bf600
saransh2320 <saransh.jain.161@gmail.com>
2020-05-13 22:43:08 +0530

    Add nextjs and Navbar

---
+++ component/Navigationbar.js
@@ -0,0 +1,30 @@
+import Head from 'next/head';
+
+import {Navbar,Nav,NavDropdown} from 'react-bootstrap';
+
+const Navigationbar = () => (
+  <div>
+    <Head>
+      <title>WriteUp</title>
+    </Head>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">WriteUp</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">Link</Nav.Link>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </div>
+)
+
+export default Navigationbar;
