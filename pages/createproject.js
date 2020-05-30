6323e5cf70885f28c588f69b19946bc390911b10
saransh2320 <saransh.jain.161@gmail.com>
2020-05-30 12:11:44 +0530

    SSR in create project and headline in [pid]

--- pages/createproject.js
+++ pages/createproject.js
@@ -12,12 +12,14 @@ import { ToastContainer, toast } from 'react-toastify';
 const url = "http://localhost:3000/api/"
 
 
-export default function Home() {
+export default function Home({ep}) {
+  console.log("ep");
+  console.log(ep);
   const router = useRouter();
   const [projectName,handleProjectName] = useState('');
   const [authorName,handleAuthorName] = useState('');
   const [aboutProject,handleAboutProject] = useState('');
-  const [existingProjects,handleExisingProject] = useState([]);
+  const [existingProjects,handleExisingProject] = useState(ep);
   const [isLoading,handleisLoading] = useState(true);
   const notifySuccess = () => toast.success("Project Created !");
 
@@ -40,20 +42,20 @@ export default function Home() {
   // Function is called only once when a component is rendered on scren
   // to get all the existing project list to save in edit Projects
 
-  useEffect(() => {
-    fetch(url+"project", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json"
-      },
-    }).then(response => response.json())
-    .then( (response)=>{
-      console.log("Get all existing Projects list");
-      handleExisingProject(response);
-      handleisLoading(false);
-      return;
-    })
-  }, []);
+  // useEffect(() => {
+  //   fetch(url+"project", {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     },
+  //   }).then(response => response.json())
+  //   .then( (response)=>{
+  //     console.log("Get all existing Projects list");
+  //     handleExisingProject(response);
+  //     handleisLoading(false);
+  //     return;
+  //   })
+  // }, []);
 
   // Following maps the JSON object to list
   const listItems = existingProjects.map((ep) =>
@@ -100,7 +102,7 @@ export default function Home() {
   return (
     <>
     <Navigationbar />
-    <div className="container">
+    <div className="container-fluid">
       <div className="row">
         <div className="col d-flex flex-column justify-content-center align-content-center">
           <div className="row justify-content-center align-content-center">
@@ -179,4 +181,69 @@ export default function Home() {
     `}</style>
     </>
   )
+}
+
+export async function getStaticProps(){
+  // const data = await [
+  //   {
+  //     _id: '5ec9542ad4f14237488f124b',
+  //     projectName: 'project 1',
+  //     authorName: 'author name 1',
+  //     __v: 0,
+  //     dateCreated: '2020-05-28T10:49:55.133Z'
+  //   },
+  //   {
+  //     _id: '5ec95c410f71a0303471eab8',
+  //     projectName: 'first project from frontend',
+  //     authorName: 'saransh',
+  //     __v: 0,
+  //     dateCreated: '2020-05-28T10:49:55.133Z'
+  //   },
+  //   {
+  //     _id: '5ec95cecc4246a022c99d9a7',
+  //     projectName: 'second project from front end',
+  //     authorName: 'saaransh',
+  //     __v: 0,
+  //     dateCreated: '2020-05-28T10:49:55.133Z'
+  //   },
+  //   {
+  //     _id: '5eca71f281fb5d284825882f',
+  //     projectName: 'new project here',
+  //     authorName: 'saransh',
+  //     __v: 0,
+  //     dateCreated: '2020-05-28T10:49:55.133Z'
+  //   },
+  //   {
+  //     _id: '5ecab70ddd85e61ea051c1fb',
+  //     projectName: 'temporary',
+  //     authorName: 'sara',
+  //     __v: 0,
+  //     dateCreated: '2020-05-28T10:49:55.133Z'
+  //   },
+  //   {
+  //     _id: '5ecd5920808c5f21f02d9a0d',
+  //     projectName: 'Date Creation added',
+  //     authorName: 'saransh',
+  //     dateCreated: '2020-05-26T18:00:00.143Z',
+  //     __v: 0
+  //   }
+  // ]
+  let response = await fetch("http://localhost:3000/api/project");
+  let data = await response.json();
+  console.log("Get all existing Projects list");
+  console.log(data);
+  return { props : {ep:data}};;
+}
+
+export async function getStaticPaths() {
+    let response = await fetch("http://localhost:3000/api/project");
+    let data = await response.json();
+
+    // Get the paths we want to pre-render based on posts
+    const paths = data.map(d => ({
+        params: {pid: d._id,"project":"project"},
+    }));
+
+    // We'll pre-render only these paths at build time.
+    return {paths, fallback: false}
 }
--- pages/project/[pid]/index.js
+++ pages/project/[pid]/index.js
@@ -4,48 +4,90 @@ import { useRouter } from 'next/router'
 import Navigationbar from "../../../Component/Navigationbar"
 import { useState , useEffect} from "react";
 
-const ProjectPage = () => {
-  const router = useRouter()
-  const { pid } = router.query
-  useEffect(() => {
-    var data = {
-      "pid" : pid,
-      "saransh":"saransh"
-    }
-    fetch("http://localhost:3000/api/project/projectbyid", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json"
-      },
-    }).then(response => response.json())
-    .then( (response)=>{
-      console.log(response);
-      return;
-    })
+function Headline({data}){
+  const [headlineData,handleHandline] = useState(data);
+  const ListofHeadline = headlineData.map( (d) =>
+    <li>
+      {d.Headline}
+    </li>
+  )
+  return (
+    <div>
+        <ul>
+          {ListofHeadline}
+        </ul>
+    </div>
+  )
+}
 
-    // .then(function(response) {
-    //   console.log(response.status)     //=> number 100–599
-    //   console.log(response.statusText) //=> String
-    //   console.log(response.headers)    //=> Headers
-    //   console.log(response.url)        //=> String
-    //   handleProjectName("");
-    //   handleAboutProject("");
-    //   handleAuthorName("");
-    //   notifySuccess();
-    //   updateExistingProject();
-    //   return response.text()
-    // }, function(error) {
-    //   console.log(error.message) //=> String
-    // })
-
-  }, []);
+const ProjectPage = ({projectData}) => {
+  // console.log("response from project page");
+  // console.log(response);
+  const router = useRouter();
+  var { pid } = router.query;
+  const [project,handleProjectData] = useState(projectData);
   return(
         <>
           <Navigationbar/>
+          <div className="continer">
+            <div className="row">
+              <div className="col-3">
+              {project.projectName}
+              </div>
+              <div className="col-7">
+              {project.authorName}
+              </div>
+              <div className="col-2">
+              {(project.dateCreated)}
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-3" style={{borderWidth:'0.5px',borderColor:'silver',borderStyle:'solid'}}>
+                <Headline data={project.data}/>
+              </div>
+              <div className="col-7" style={{borderWidth:'0.5px',borderColor:'silver',borderStyle:'solid'}}>
+                 Content
+              </div>
+              <div className = "col-2" style={{borderWidth:'0.5px',borderColor:'silver',borderStyle:'solid'}}>
+                Extra
+              </div>
+            </div>
+          </div>
         </>
   )
 }
 
+export async function getStaticProps({params}) {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  var data = {
+    "pid" : params.pid,
+  }
+  console.log("called for get static props");
+  console.log(data);
+  let response = await fetch("http://localhost:3000/api/project/projectbyid", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    },
+  })
+  let dt = await response.json();
+  console.log("+++++++++++",dt);
+  return { props : {projectData:dt}};
+
+}
+export async function getStaticPaths() {
+    let response = await fetch("http://localhost:3000/api/project");
+    let data = await response.json();
+
+    // Get the paths we want to pre-render based on posts
+    const paths = data.map(d => ({
+        params: {pid: d._id,"project":"project"},
+    }));
+
+    // We'll pre-render only these paths at build time.
+    return {paths, fallback: false}
+}
 
 export default ProjectPage
