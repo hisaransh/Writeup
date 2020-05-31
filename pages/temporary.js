8cfc8dc0654ebe551195c7b9285ba50569be2dde
saransh2320 <saransh.jain.161@gmail.com>
2020-06-01 00:33:32 +0530

    experimenting slatejs on temporary

--- pages/temporary.js
+++ pages/temporary.js
@@ -1,28 +1,28 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import { ToastContainer, toast } from 'react-toastify';
+import React, { useEffect, useMemo, useState } from "react";
+// Import the Slate editor factory.
+import { createEditor } from 'slate'
 
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from 'slate-react'
 
 export default function Temp(){
    const notify = () => toast.success("Project Created !");
+   const editor = useMemo(() => withReact(createEditor()), [])
+   // Keep track of state for the value of the editor.
+   const [value, setValue] = useState([
+    {
+      type: 'paragraph',
+      children: [{ text: 'A line of text in a paragraph.' }],
+    },
+  ])
   return(
     <div>
-      <h1>You are in temporary</h1>
-      <h2>All experiments regarding frontend can be done here</h2>
-      <div>
-        
-        <ToastContainer
-          position="top-right"
-          autoClose={2500}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-      </div>
+        <Slate editor={editor} value={value} onChange={newValue => setValue(newValue)}>
+          <Editable />
+        </Slate>
     </div>
   )
 }
